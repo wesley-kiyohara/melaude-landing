569953b040867d0dbf22bd24cdd9c5bb4c698d81
Wesley Kiyohara <wesleykiyohara@gmail.com>
2014-08-15 01:18:26 -0700

    need to figure out overflow/position absolute problem

--- public/beta-signup/scripts/main.js
+++ public/beta-signup/scripts/main.js
@@ -38,7 +38,7 @@
             message = 0;
         }
     };
-    $('#message').on('webkitAnimationIteration', changeMessage);
+    $('#message').on('animationIteration webkitAnimationIteration mozAnimationIteration oAnimationIteration', changeMessage);
     /**
      * Success callback for submitting emails for leads.  Let's user know of the result.
      * @param  {Object} result Contains an "errors" property if email is invalid.
