dbb566f5d1b7ad13c0eb420760052d1796c2d469
Wesley Kiyohara <wesleykiyohara@gmail.com>
2014-09-10 09:57:25 -0700

    added second implementation of message changing

--- public/beta-signup/scripts/main.js
+++ public/beta-signup/scripts/main.js
@@ -27,6 +27,7 @@
 
     var message1 = 1;
     var message2 = 1;
+    var lastMessage = 0;
 
     var callbacks = {
         success: {},
@@ -36,35 +37,78 @@
     //changeMessage1() is called when #message1's animation happens
     //changeMessage2() is called when #message2's animation happens (3s delay from #message1)
     var changeMessage1 = function(){
-        if (message1 === 0){
-            $('#message1').text('IN YOUR NEIGHBORHOOD');
-            message1 = 1;
-        }
-        else if (message1 === 1){
-            $('#message1').text('AROUND THE WORLD');
-            message1 = 2;
-        }else{
-            $('#message1').text('ACROSS THE COUNTRY');
-            message1 = 0;
+        if (lastMessage != 1){
+            console.log('trigger1');
+            if (message1 === 0){
+                $('#message1').text('IN YOUR NEIGHBORHOOD');
+                message1 = 1;
+            }
+            else if (message1 === 1){
+                $('#message1').text('AROUND THE WORLD');
+                message1 = 2;
+            }else{
+                $('#message1').text('ACROSS THE COUNTRY');
+                message1 = 0;
+            }
+            lastMessage = 1;
         }
     };
     var changeMessage2 = function(){
-        if (message2 === 0){
-            $('#message2').text('ACROSS THE COUNTRY');
-            message2 = 1;
-        }
-        else if (message2 === 1){
-            $('#message2').text('IN YOUR NEIGHBORHOOD');
-            message2 = 2;
-        }else{
-            $('#message2').text('AROUND THE WORLD');
-            message2 = 0;
+        if (lastMessage != 2){
+            console.log('trigger2');
+            if (message2 === 0){
+                $('#message2').text('ACROSS THE COUNTRY');
+                message2 = 1;
+            }
+            else if (message2 === 1){
+                $('#message2').text('IN YOUR NEIGHBORHOOD');
+                message2 = 2;
+            }else{
+                $('#message2').text('AROUND THE WORLD');
+                message2 = 0;
+            }
+            lastMessage = 2;
         }
     };
     //Call changeMessage1() or changeMessage2() when a CSS animation happens to keep CSS and JS synched
-    $('#message1').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', changeMessage1);
-    $('#message2').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', changeMessage2);
-    
+    //$('#message1').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', changeMessage1);
+    //$('#message2').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', changeMessage2);
+    $('.message-wrapper').on('webkitAnimationIteration', function(event){
+        if (event.originalEvent.animationName === 'fadein1'){
+            if (lastMessage != 1){
+                console.log('trigger1');
+                if (message1 === 0){
+                    $('#message1').text('IN YOUR NEIGHBORHOOD');
+                    message1 = 1;
+                }
+                else if (message1 === 1){
+                    $('#message1').text('AROUND THE WORLD');
+                    message1 = 2;
+                }else{
+                    $('#message1').text('ACROSS THE COUNTRY');
+                    message1 = 0;
+                }
+                lastMessage = 1;
+            }
+        }else{
+            if (lastMessage != 2){
+                console.log('trigger2');
+                if (message2 === 0){
+                    $('#message2').text('ACROSS THE COUNTRY');
+                    message2 = 1;
+                }
+                else if (message2 === 1){
+                    $('#message2').text('IN YOUR NEIGHBORHOOD');
+                    message2 = 2;
+                }else{
+                    $('#message2').text('AROUND THE WORLD');
+                    message2 = 0;
+                }
+                lastMessage = 2;
+            }
+        }
+    });
+
     /**
      * Success callback for submitting emails for leads.  Let's user know of the result.
      * @param  {Object} result Contains an "errors" property if email is invalid.
