07c6b0af38c6b888c5a3b840bd541278793889f5
Kevin Sison <kevinsison@fb.com>
2014-09-28 20:20:41 -0700

    Mailchimp subscription confirmation email implementation.

--- server/routes/lead.route.js
+++ server/routes/lead.route.js
@@ -1,6 +1,50 @@
 'use strict';
 
-var Lead = require('../models/lead.model');
+var Lead = require('../models/lead.model'),
+    request = require('request'),
+    config = require('../config/config');
+
+function saveLeadCallback(email, res) {
+    return function(err) {
+        if (err) {
+            res.status(400).json([err]);
+        }
+        else {
+
+            // Subscribes email to newsletter (mailchimp) list.
+            subscribe(email, subscribeCallback(res));
+        }
+    };
+}
+
+function subscribe(email, callback) {
+    request({
+        url: 'https://us9.api.mailchimp.com/2.0/lists/subscribe',
+        method: 'POST',
+        form: {
+            apikey: config.mailchimp.apiKey,
+            id: config.mailchimp.listId,
+            email: {
+                email: email
+            }
+        }
+    }, callback);
+}
+
+function subscribeCallback(res) {
+    return function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+
+            // Send a response message that the lead was successfully created and added to mailchimp list. Needs to be
+            // valid JSON for ajax request to succeed.
+            res.status(200).json({ status: 'ok' });
+        }
+        else {
+            res.status(400).json([err]);
+        }
+    };
+}
+
 
 module.exports = function(app) {
 
@@ -12,7 +56,7 @@ module.exports = function(app) {
         .post(function(req, res) {
 
             // Validate email from request
-            req.assert('email', 'A valid email is required').isEmail();
+            req.assert('email', 'A valid e/mail is required').isEmail();
 
             var errors = req.validationErrors(),
                 lead;
@@ -26,18 +70,7 @@ module.exports = function(app) {
                 lead.email = req.body.email;
 
                 // Save new lead into database.
-                lead.save(function(err) {
-
-                    if (err) {
-                        res.status(400).json([err]);
-                    }
-                    else {
-
-                         // Send a response message that the lead was successfully created. Needs to be
-                         // valid JSON for ajax request to succeed.
-                        res.status(200).json({ status: 'ok' });
-                    }
-                });
+                lead.save(saveLeadCallback(lead.email, res));
             }
             else {
 
