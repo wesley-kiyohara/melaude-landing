2d42dff1dd381f1c013a4f39745a6ad4dbd76079
Kevin Sison <kevinsison@fb.com>
2014-09-28 18:41:30 -0700

    Dynamic NODE_ENV configs for database.

--- server/config/env/default.js
+++ server/config/env/default.js
@@ -6,8 +6,16 @@ var rootPath = path.normalize(__dirname + '/../../..');
 module.exports = {
     root: rootPath,
     port: process.env.PORT || 3000,
-    hostname: process.env.HOST || process.env.HOSTNAME,
-    db: process.env.MONGOHQ_URL || 'mongodb://melaude:melaude@kahana.mongohq.com:10031/melaude_landing_development'
-    // db: process.env.MONGOHQ_URL || 'mongodb://melaude:melaude@kahana.mongohq.com:10030/melaude_landing_testing'
-    // db: process.env.MONGOHQ_URL || 'mongodb://melaude:melaude@kahana.mongohq.com:10029/melaude_landing_production'
+    hostname: process.env.HOST || process.env.HOSTNAME
 };
+
+switch (process.env.NODE_ENV) {
+    case 'production':
+        module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10029/melaude_landing_production';
+        break;
+    case 'testing':
+        module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10030/melaude_landing_testing';
+        break;
+    default:
+        module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10031/melaude_landing_development';
+}
