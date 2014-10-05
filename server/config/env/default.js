0ec7cdccf4fcb8e625064682c84bca59d86f373b
Kevin Sison <kevinsison@fb.com>
2014-10-05 12:45:57 -0700

    Fixed thank you message.  Added multiple mailchimp lists for the different environments.

--- server/config/env/default.js
+++ server/config/env/default.js
@@ -8,18 +8,20 @@ module.exports = {
     port: process.env.PORT || 3000,
     hostname: process.env.HOST || process.env.HOSTNAME,
     mailchimp: {
-        apiKey: '52b2db3ff7056b2ec6c43e989bc1efde-us9',
-        listId: 'dab7b3ce07'
+        apiKey: '52b2db3ff7056b2ec6c43e989bc1efde-us9'
     }
 };
 
 switch (process.env.NODE_ENV) {
     case 'production':
         module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10029/melaude_landing_production';
+        module.exports.mailchimp.listId = 'dab7b3ce07';
         break;
     case 'testing':
         module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10030/melaude_landing_testing';
+        module.exports.mailchimp.listId = '5d309597d9';
         break;
     default:
         module.exports.db = 'mongodb://melaude:melaude@kahana.mongohq.com:10031/melaude_landing_development';
+        module.exports.mailchimp.listId = '1549de63af';
 }
